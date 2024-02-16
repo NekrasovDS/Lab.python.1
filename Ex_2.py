1b01a5b49c00e023707a7d52cc5c4eb3cb745514
NekrasovDS <112845788+NekrasovDS@users.noreply.github.com>
2024-02-16 12:06:10 +0300

    Update Ex_2.py

--- Ex_2.py
+++ Ex_2.py
@@ -1,1 +1,8 @@
+#Задание 2. Вариант 8.
+#Задача 2.
+def check_ordered(string):
+    lowercase_chars = [char for char in string if char.islower()]
+    sorted_chars = sorted(lowercase_chars)
+    return lowercase_chars == sorted_chars
+
 
