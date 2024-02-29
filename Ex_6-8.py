ef0962cd1e7ffa87820d5bda7992c2fd48da5c9e
NekrasovDS <112845788+NekrasovDS@users.noreply.github.com>
2024-02-29 22:25:53 +0300

    Update Ex_6-8.py

--- Ex_6-8.py
+++ Ex_6-8.py
@@ -17,3 +17,18 @@ def get_filename_without_extension(file_path):
     filename = os.path.basename(file_path)
     filename_without_extension = os.path.splitext(filename)[0]
     return filename_without_extension
+
+#Ввод и вывод
+number = int(input('Для решения определённой задачи, введите её номер (предложенные задачи варианта: 2, 10, 17): '))
+if number == 2:
+    text = input('Введите желаемую строку: ')
+    result = find_lowercase_letters(text)
+    print(result)
+if number == 10:
+    input_string = input('Введите желаемую строку: ')
+    result = count_unique_latin_chars(input_string)
+    print(result)
+if number == 17:
+    file_path = input('Введите путь: ')
+    filename = get_filename_without_extension(file_path)
+    print (filename)
