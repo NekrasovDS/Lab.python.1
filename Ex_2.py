bc83f029f8a771f28db0bb618d1cca3278570d49
NekrasovDS <112845788+NekrasovDS@users.noreply.github.com>
2024-02-29 21:24:44 +0300

    Update Ex_2.py

--- Ex_2.py
+++ Ex_2.py
@@ -17,3 +17,17 @@ def get_filename_without_extension(file_path):
     filename = os.path.basename(file_path)
     filename_without_extension = os.path.splitext(filename)[0]
     return filename_without_extension
+
+number = int(input('Для решения определённой задачи, введите её номер (предложенные задачи варианта: 2, 10, 17): '))
+if number == 2:
+    string = input('Введите желаемую строку: ')
+    ordered = check_ordered(string)
+    print(ordered)
+if number == 10:
+    string = input('Введите желаемую строку: ')
+    a_count = count_letter_A(string)
+    print(a_count)
+if number == 17:
+    file_path = input('Введите путь: ')
+    filename = get_filename_without_extension(file_path)
+    print (filename)
