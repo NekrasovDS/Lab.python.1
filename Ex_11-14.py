70309bc0c558b752b79cb316d1fc5cc92eece2da
NekrasovDS <112845788+NekrasovDS@users.noreply.github.com>
2024-03-01 12:09:50 +0300

    Update Ex_11-14.py

--- Ex_11-14.py
+++ Ex_11-14.py
@@ -1,9 +1,9 @@
 #Вариант 8. Задание 11-14.
-#Задание 2.
+#Задача 2.
 def sort_strings_by_avg_weight(strings):
     return sorted(strings, key=lambda x: sum(ord(char) for char in x)/len(x))
 
-#Задание 4.
+#Задача 4.
 def sort_by_quadratic_deviation(strings):
     ref_avg = sum(ord(char) for char in strings[0])/len(strings[0])
     return sorted(strings, key=lambda x: (sum(ord(char) for char in x)/len(x) - ref_avg)**2)
