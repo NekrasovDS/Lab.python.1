c3913786e3e0604554244a9275e3a2d489ad49dd
NekrasovDS <112845788+NekrasovDS@users.noreply.github.com>
2024-03-29 11:53:37 +0300

    Update Ex_15-19.py

--- Ex_15-19.py
+++ Ex_15-19.py
@@ -17,4 +17,10 @@ def find_missing_numbers(arr):
       missing.append(i)
   return missing
 
-
+#Задача 32.
+def count_local_maxima(arr):
+  count = 0
+  for i in range(1, len(arr) - 1):
+    if arr[i] > arr[i - 1] and arr[i] > arr[i + 1]:
+      count += 1
+return count
