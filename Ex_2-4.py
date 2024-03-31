a5df91d677ca18ede24cf22b5627b0163cbd7a95
NekrasovDS <112845788+NekrasovDS@users.noreply.github.com>
2024-03-31 18:46:18 +0300

    Update Ex_2-4.py

--- Ex_2-4.py
+++ Ex_2-4.py
@@ -1,11 +1,11 @@
-#Задание 2. Вариант 8.
-#Задача 2.
+# Задание 2. Вариант 8.
+# Задача 2.
 def check_ordered(string):
     lowercase_chars = [char for char in string if char.islower()]
     sorted_chars = sorted(lowercase_chars)
     return lowercase_chars == sorted_chars
 
-#Задача 10
+# Задача 10
 def count_letter_A(string):
     count = 0
     for letter in string:
@@ -13,13 +13,13 @@ def count_letter_A(string):
             count +=1
     return count
 
-#Задача 17
+# Задача 17
 def get_filename_without_extension(file_path):
     filename = os.path.basename(file_path)
     filename_without_extension = os.path.splitext(filename)[0]
     return filename_without_extension
 
-#Ввод и вывод
+# Ввод и вывод
 number = int(input('Для решения определённой задачи, введите её номер (предложенные задачи варианта: 2, 10, 17): '))
 if number == 2:
     string = input('Введите желаемую строку: ')
