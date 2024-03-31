146902963468301a43e10a2e51b871077fce24fd
NekrasovDS <112845788+NekrasovDS@users.noreply.github.com>
2024-03-31 18:46:50 +0300

    Update Ex_6-8.py

--- Ex_6-8.py
+++ Ex_6-8.py
@@ -1,5 +1,5 @@
-#Задание 6-8. Вариант 8.
-#Функция 1.
+# Задание 6-8. Вариант 8.
+# Функция 1.
 def find_lowercase_letters(text):
     lowercase_letters = set()
     for char in text:
@@ -7,18 +7,18 @@ def find_lowercase_letters(text):
             lowercase_letters.add(char)
     return lowercase_letters
 
-#Функция 2.
+# Функция 2.
 def count_unique_latin_chars(input_string):
     latin_chars = set(char for char in input_string if char.isalpha() and char.isascii() and char.islower())
     return len(latin_chars)
 
-#Функция 3.
+# Функция 3.
 def get_filename_without_extension(file_path):
     filename = os.path.basename(file_path)
     filename_without_extension = os.path.splitext(filename)[0]
     return filename_without_extension
 
-#Ввод и вывод
+# Ввод и вывод
 number = int(input('Для решения определённой задачи, введите её номер (предложенные задачи варианта: 2, 10, 17): '))
 if number == 2:
     text = input('Введите желаемую строку: ')
